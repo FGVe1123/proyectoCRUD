930e632fe072dcf48da2f70387680e0f827c3a2f
Victor Flores <victor_flores.g@outlok.com>
2022-10-24 00:00:47 -0500

    metodos completados v1

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
+
 class ProductoController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
+     * Muestra lista de los recursos
+     * Muestra vista, se necesita acceso a los elementos almacenados ($productos)
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -18,9 +19,11 @@ class ProductoController extends Controller
         return view('productos.productosIndex', compact('productos'));
     }
 
+
+
     /**
-     * Show the form for creating a new resource.
-     *
+     * Muestra formulario para la creación de nuevos recursos
+     * Vista con formulario (el almacenamiento va aparte)
      * @return \Illuminate\Http\Response
      */
     public function create()
@@ -28,9 +31,13 @@ class ProductoController extends Controller
         return view('productos.productosCreate');
     }
 
+
+
     /**
-     * Store a newly created resource in storage.
-     *
+     * almacena el nuevo recurso
+     * Se aplican validaciones (en caso de ser necesarias, tener en cuenta las validaciones hechas en las migraciones)
+     *  los elementos del formulario tiene que almacenarse en la base de datos
+     *  mandar a una vista
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -44,33 +51,47 @@ class ProductoController extends Controller
         ]);
         Producto::create($request->all());
      
-        return view('/welcome');
+        return view('productos.productosIndex');
     }
 
+
+
+
     /**
-     * Display the specified resource.
-     *
+     * Se muestran los detalles del recurso (descripción*)
+     * Se espera algún parámetro en la URL (ID*)
+     * la manera de activar este método dependerá del diseño de la página
      * @param  \App\Models\Producto  $producto
      * @return \Illuminate\Http\Response
      */
     public function show(Producto $producto)
     {
-        //
+        return view('producto.productosShow', compact('producto'));
     }
 
+
+
     /**
-     * Show the form for editing the specified resource.
+     * Se muestra el formulario para editar un recurso, puede ser un solo formulario para crear y editar
+     *  cada formulario debe actuar de manera distinta por la acción que realizan
+     * 
+     * Requiere agregar @ method('patch') para su funcionamiento.
+     * tener alguna forma de activar el método (botón, enlace...)
      *
      * @param  \App\Models\Producto  $producto
      * @return \Illuminate\Http\Response
      */
     public function edit(Producto $producto)
     {
-        //
+        return view('productos.productosEdit', compact('producto'));
     }
 
+
+
     /**
-     * Update the specified resource in storage.
+     * Guarda los cambios realizados en el formulario editar.
+     * se pueden aplicar las mismas reglas de validacion si se requieren
+     * por ser actualización de datos se utiliza "save" (no es creación de recurso)
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Producto  $producto
@@ -78,17 +99,39 @@ class ProductoController extends Controller
      */
     public function update(Request $request, Producto $producto)
     {
-        //
+        $request -> validate([
+            'existencia' => 'required',
+            'nombre' => 'required|max255',
+            'modelo' => ' required',
+            'precio' => 'required',
+        ]);
+
+        $producto->existencia = $request->existencia;
+        $producto-> nombre= $request->nombre;
+        $producto-> modelo= $request->modelo;
+        $producto-> precio= $request->precio;
+
+        $producto->save();
+
+        return redirect ('productos.productosIndex');
+
     }
 
+
+
     /**
-     * Remove the specified resource from storage.
+     * Remover algún recurso (eliminar)
+     * Se tiene que hacer la petición POST
+     *      Requiere el metodo @ csrf
+     *      requiere @ method('delete')
      *
      * @param  \App\Models\Producto  $producto
      * @return \Illuminate\Http\Response
      */
     public function destroy(Producto $producto)
     {
-        //
+        $producto->delete();
+
+        return redirect(productos.productosIndex);
     }
 }
